6b34ad8a5facbd69ba66a21a2054bc9ac5efafe3
Sri Ram Kumar <ramkumar10950@gmail.com>
2022-11-27 12:04:23 +0530

    Update Utility.js

--- Utility.js
+++ Utility.js
@@ -1,19 +1,19 @@
 /**
- * It prevents the user from entering anything other than numbers, backspace, delete, arrow keys,
- * enter, and tab
+ * It prevents the user from entering anything other than numbers, the decimal point, the backspace
+ * key, the arrow keys, the enter key, and the tab key
  * @param event - The event object is a JavaScript event that is sent to an element when an event
- * occurs.
+ * occurs on the element.
  */
 function inputNumber(event) {
+    let keyArray = [110, 190, 8, 13, 9];
     const keyCode = event.keyCode;
-    if ((keyCode >= 48 && keyCode <= 57) || keyCode == 188 || keyCode == 8 || (keyCode >= 96 && keyCode <= 105) || (keyCode >= 37 && keyCode <= 40) || keyCode == 13 || keyCode == 9) {
-    } else {
+    if (!((keyCode >= 48 && keyCode <= 57) || keyArray.includes(keyCode) || (keyCode >= 96 && keyCode <= 105) || (keyCode >= 37 && keyCode <= 40))) {
         event.preventDefault();
     }
 }
 /**
  * It takes an array of objects and returns a string of the objects' label and value properties
- * @param obj - The object to be converted to a string.
+ * @param {string}obj - The object to be converted to a string.
  */
 function objToString(obj) {
     let str = '';
@@ -21,4 +21,33 @@ function objToString(obj) {
         str += element.label + ' ' + element.value + '\n ';
     });
     return str;
+}
+
+/**
+ * It takes an array of objects, sorts them by their label property, and returns the sorted array
+ * @param {String}obj - The object you want to sort.
+ * @returns The sortedObj array.
+ */
+function ObjectSort(obj) {
+    let sortedObj = [];
+    sortedObj = obj.sort((a, b) => a.label.localeCompare(b.label));
+    return sortedObj;
+}
+
+/**
+ * It takes a selector as an argument and returns true if all the input fields that match the selector
+ * are valid
+ * @param selector - The selector for the input fields you want to validate.
+ * @returns A boolean value.
+ */
+function isInputValid(selector) {
+  let isValid = true;
+  let inputFields = this.template.querySelectorAll(selector);
+  inputFields.forEach(inputField => {
+    if (!inputField.checkValidity()) {
+      inputField.reportValidity();
+      isValid = false;
+    }
+  });
+  return isValid;
 }
