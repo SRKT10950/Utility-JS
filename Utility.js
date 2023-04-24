6a093897272c60cc4aed2c1fb0b31f22151db55d
Sri Ram Kumar <ramkumar10950@gmail.com>
2023-04-24 17:33:11 +0530

    dateValidation

--- Utility.js
+++ Utility.js
@@ -33,6 +33,31 @@ function ObjectSort(obj) {
     sortedObj = obj.sort((a, b) => a.label.localeCompare(b.label));
     return sortedObj;
 }
+ /**
+     * The function checks if a given date string is valid or not.
+     * @param dateString - a string representing a date in the format "dd/mm/yyyy" (day/month/year)
+     * @returns a boolean value indicating whether the input string is a valid date in the format
+     * "dd/mm/yyyy".
+     */
+    dateValidation(dateString) {
+        // First check for the pattern
+        if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+            return false;
+        // Parse the date parts to integers
+        let parts = dateString.split("/");
+        let day = parseInt(parts[0], 10);
+        let month = parseInt(parts[1], 10);
+        let year = parseInt(parts[2], 10);
+        // Check the ranges of month and year
+        if (year < 1000 || year > 3000 || month == 0 || month > 12)
+            return false;
+        let monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        // Adjust for leap years
+        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+            monthLength[1] = 29;
+        // Check the range of the day
+        return day > 0 && day <= monthLength[month - 1];
+    }
 
 /**
  * It takes a selector as an argument and returns true if all the input fields that match the selector
